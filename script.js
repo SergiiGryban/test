47263413b5c1eeed2a26298e3cf1a423eeff4377
Sergiy <sgriban7@gmail.com>
2020-06-30 10:20:10 +0300

    Это мой первый комит

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+
+var leftBorderWidth =1;
+let second =2;
+const pi =3.14;
+
+console.log(second);
+console.log(leftBorderWidth);
+
+console.log('Hello Petya')
+
+let arr = ['ripl.png', 'bitoc.rpg', 'ymahz.jps'];
+console.log(arr[0]);
+
+//alert('Hello randomchik');
+
+// let ansver = confirm('Vi tuta?');
+// console.log(ansver);
+
+//let ansver = prompt('Есть ли вам 18?', 'Yes');
+
+// console.log(typeof(ansver));
+// console.log(typeof(arr));
+
+
+console.log('arr'+'-odject');
+
